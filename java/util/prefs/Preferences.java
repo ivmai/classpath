59ba67028a7c2e9f108c6f4a2457d58333995caf
Tom Tromey <tromey@redhat.com>
2005-10-06 00:38:41 +0000

    * java/util/prefs/Preferences.java (systemNodeForPackage): Genericized. 	(userNodeForPackage): Likewise.

--- java/util/prefs/Preferences.java
+++ java/util/prefs/Preferences.java
@@ -242,7 +242,7 @@ public abstract class Preferences {
      * @exception SecurityException when a security manager is installed and
      * the caller does not have <code>RuntimePermission("preferences")</code>.
      */
-    public static Preferences systemNodeForPackage(Class c)
+    public static Preferences systemNodeForPackage(Class<?> c)
             throws SecurityException
     {
         return nodeForPackage(c, systemRoot());
@@ -261,7 +261,7 @@ public abstract class Preferences {
      * @exception SecurityException when a security manager is installed and
      * the caller does not have <code>RuntimePermission("preferences")</code>.
      */
-    public static Preferences userNodeForPackage(Class c)
+    public static Preferences userNodeForPackage(Class<?> c)
             throws SecurityException
     {
         return nodeForPackage(c, userRoot());
