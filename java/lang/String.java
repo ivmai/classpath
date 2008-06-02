f96acb3407597642d4ce0ca828c8e39db128e10f
Andrew John Hughes <gnu_andrew@member.fsf.org>
2008-06-02 20:38:55 +0000

    Backport fix for PR35482.
    2008-05-06  Andrew John Hughes  <gnu_andrew@member.fsf.org>
    
    	PR classpath/35482
    	* java/lang/String.java:
    	(toLowerCase()): Fix calculation of number
    	of characters to copy.
    	(toLowerCaseTurkish()): Likewise.

--- java/lang/String.java
+++ java/lang/String.java
@@ -1451,7 +1451,7 @@ public final class String
     // Now we perform the conversion. Fortunately, there are no multi-character
     // lowercase expansions in Unicode 3.0.0.
     char[] newStr = new char[count];
-    VMSystem.arraycopy(value, offset, newStr, 0, count - (x - offset));
+    VMSystem.arraycopy(value, offset, newStr, 0, x - offset);
     do
       {
         char ch = value[x];
@@ -1507,7 +1507,7 @@ public final class String
         // Now we perform the conversion. Fortunately, there are no
         // multi-character lowercase expansions in Unicode 3.0.0.
         char[] newStr = new char[count];
-        VMSystem.arraycopy(value, offset, newStr, 0, count - (x - offset));
+        VMSystem.arraycopy(value, offset, newStr, 0, x - offset);
         do
           {
             char ch = value[x];
